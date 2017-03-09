0090c15f6a51d876ef0b44b0806a16ba4c455a02
PJB3005 <pieterjan.briers+git@gmail.com>
2017-03-09 21:53:08 +0100

    Initial Commit

---
+++ Source/ThreeLanes/ThreeLanes.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThreeLanes.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThreeLanes, "ThreeLanes" );
---
+++ Source/ThreeLanes/ThreeLanesGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ThreeLanesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THREELANES_API AThreeLanesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
